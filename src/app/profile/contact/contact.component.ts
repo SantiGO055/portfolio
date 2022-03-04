8acb63c4b116e89d60eff1883a27fab27fa2d030
Santiago Gonzalez <40776497+SantiGO055@users.noreply.github.com>
2022-03-04 12:02:25 -0300

    rc

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -29,7 +29,7 @@ export class ContactComponent implements OnInit {
       'texto': ['',[Validators.required]],
       
       'email': ['',[Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
-      'captcha': ['',Validators.required],
+      //'captcha': ['',Validators.required],
       
     });
   }
