fb083b61a47d1482f1d3f25600244bc09069d1f6
SantiGO055 <santigonzalez05@gmail.com>
2022-06-08 17:08:19 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,33 +9,33 @@ import { GithubCalendarComponent } from './profile/github-calendar/github-calend
 import { ProfileModule } from './profile/profile.module';
 import { GithubRepoCardComponent } from './profile/projects/github-repo-card/github-repo-card.component';
 import { GraphQLModule } from './profile/projects/graphql.module';
-import { RecaptchaFormsModule, RecaptchaModule, ReCaptchaV3Service, RecaptchaV3Module, RECAPTCHA_V3_SITE_KEY  } from 'ng-recaptcha';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { RecaptchaFormsModule, RecaptchaModule, ReCaptchaV3Service, RecaptchaV3Module, RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    
-    
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ProfileModule,
-    
+
     HttpClientModule,
     GraphQLModule,
     AnimateModule,
     RecaptchaFormsModule,
-    
+
     RecaptchaModule,
     RecaptchaV3Module,
     BrowserAnimationsModule
-    
-    
-    
-    
+
+
+
+
   ],
   providers: [ReCaptchaV3Service, { provide: Window, useValue: window }],
   bootstrap: [AppComponent],
