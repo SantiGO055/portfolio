b7109c58317f1ec1e4116d59e2e381fc414a15c7
SantiGO055 <santigonzalez05@gmail.com>
2023-08-08 09:16:10 -0300

    .

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -8,44 +8,47 @@ import { EmailService } from 'src/app/services/email.service';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-  public formGroup!: FormGroup;
-
-  public contactInfo = {
-    title: "Contactame ☎️",
-    subtitle: "Completa el formulario para contactarme.",
-    
-  };
-  constructor(
-    private fb:FormBuilder,
-    private emailjs: EmailService
-
-  ) { }
-
   ngOnInit(): void {
-    this.formGroup = this.fb.group({
-      'nombre': ['',[Validators.required]],
-      // 'tipo': ['',Validators.required],
-      'telefono': [''],
-      'texto': ['',[Validators.required]],
-      
-      'email': ['',[Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
-      //'captcha': ['',Validators.required],
-      
-    });
+    throw new Error('Method not implemented.');
   }
-  enviarMail(){
-    console.log(this.formGroup.get('texto')?.value,this.formGroup.get('email')?.value,this.formGroup.get('nombre')?.value,this.formGroup.get('telefono')?.value)
-    let mensaje = this.formGroup.get('texto')?.value;
-    let email = this.formGroup.get('email')?.value;
-    let nombre = this.formGroup.get('nombre')?.value;
-    let telefono = this.formGroup.get('telefono')?.value;
-    if(mensaje && email && nombre && telefono){
-      this.formGroup.reset()
-    }
+  // public formGroup!: FormGroup;
 
-    this.emailjs.sendEmail(mensaje,email,nombre,telefono);
+  // public contactInfo = {
+  //   title: "Contactame ☎️",
+  //   subtitle: "Completa el formulario para contactarme.",
 
-  }
-    
+  // };
+  // constructor(
+  //   private fb:FormBuilder,
+  //   private emailjs: EmailService
+
+  // ) { }
+
+  // ngOnInit(): void {
+  //   this.formGroup = this.fb.group({
+  //     'nombre': ['',[Validators.required]],
+  //     // 'tipo': ['',Validators.required],
+  //     'telefono': [''],
+  //     'texto': ['',[Validators.required]],
+
+  //     'email': ['',[Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+  //     //'captcha': ['',Validators.required],
+
+  //   });
+  // }
+  // enviarMail(){
+  //   console.log(this.formGroup.get('texto')?.value,this.formGroup.get('email')?.value,this.formGroup.get('nombre')?.value,this.formGroup.get('telefono')?.value)
+  //   let mensaje = this.formGroup.get('texto')?.value;
+  //   let email = this.formGroup.get('email')?.value;
+  //   let nombre = this.formGroup.get('nombre')?.value;
+  //   let telefono = this.formGroup.get('telefono')?.value;
+  //   if(mensaje && email && nombre && telefono){
+  //     this.formGroup.reset()
+  //   }
+
+  //   this.emailjs.sendEmail(mensaje,email,nombre,telefono);
+
+  // }
+
 
 }
